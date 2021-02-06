180499dc4eda4d6b8dd0ba57181bfe5ad5e3d0ee
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2021-02-06 18:56:04 +0100

    Removed some prunes.

--- sources/utilities.lisp
+++ sources/utilities.lisp
@@ -24,8 +24,8 @@
 
 ;; Structures.
 
-(defstruct (line-struct (:constructor make-cable))
-  (name (symbol-name (gensym "cable-")) :type (or symbol keyword string (unsigned-byte 64)))
+(defstruct (line-struct (:constructor make-line))
+  (name (symbol-name (gensym "line-")) :type (or symbol keyword string (unsigned-byte 64)))
   (conductor-length 0d0 :type real)
   (conductor-resistivity 0d0 :type real)
   (conductor-formation 1 :type (integer 1))
@@ -38,7 +38,7 @@
 ;; Funtions
 
 (defun make-line (&rest parameters &key
-                                     (name (symbol-name (gensym "cable-")) name-p)
+                                     (name (symbol-name (gensym "line-")) name-p)
                                      (conductor-length 1d0 conductor-length-p)
                                      (conductor-resistivity 1.78d-8 conductor-resistivity-p)
                                      (conductor-formation 1 conductor-formation-p)
@@ -77,81 +77,87 @@
       (check-type c real)))
   (assert (= (length conductor-cross-sections)
              (length conductor-positions)))
-  (let ((object (allocate-instance (find-class 'cable-struct))))
-    (setf (cable-struct-name object) name
-          (cable-struct-conductor-length object) conductor-length
-          (cable-struct-conductor-resistivity object) conductor-resistivity
-          (cable-struct-conductor-formation object) conductor-formation
-          (cable-struct-insulation-permittivity object) insulation-permittivity
-          (cable-struct-insulation-permeability object) insulation-permeability
-          (cable-struct-conductor-positions object) conductor-positions
-          (cable-struct-conductor-cross-sections object) conductor-cross-sections)
+  (let ((object (allocate-instance (find-class 'line-struct))))
+    (setf (line-struct-name object) name
+          (line-struct-conductor-length object) conductor-length
+          (line-struct-conductor-resistivity object) conductor-resistivity
+          (line-struct-conductor-formation object) conductor-formation
+          (line-struct-insulation-permittivity object) insulation-permittivity
+          (line-struct-insulation-permeability object) insulation-permeability
+          (line-struct-conductor-positions object) conductor-positions
+          (line-struct-conductor-cross-sections object) conductor-cross-sections)
     object))
 
                          
 (defun make-line-model (&rest parameters &key
-                                            (cable nil cable-p))
-  "Create a cable model."
-  (declare (ignorable parameters cable))
-  (when cable-p
-    (check-type cable cable-struct))
+                                           (line nil line-p)
+                                           (frequency 50d0 frequency-p))
+  "Create a line model."
+  (declare (ignorable parameters line frequency))
+  (when line-p
+    (check-type line line-struct))
+  (when frequency-p
+    (check-type frequency real))
   (let* ((a-matrix (grid:make-foreign-array 'double-float
-                                            :dimensions (list (length (cable-struct-conductor-positions cable))
-                                                              (length (cable-struct-conductor-positions cable)))
+                                            :dimensions (list (length (line-struct-conductor-positions line))
+                                                              (length (line-struct-conductor-positions line)))
                                             :initial-element 0d0))
          (m-matrix (grid:make-foreign-array 'double-float
-                                            :dimensions (list (length (cable-struct-conductor-positions cable))
-                                                              (length (cable-struct-conductor-positions cable)))
+                                            :dimensions (list (length (line-struct-conductor-positions line))
+                                                              (length (line-struct-conductor-positions line)))
                                             :initial-element 0d0))
          (d-matrix (grid:make-foreign-array 'double-float
-                                            :dimensions (list (length (cable-struct-conductor-positions cable))
-                                                              (length (cable-struct-conductor-positions cable)))
+                                            :dimensions (list (length (line-struct-conductor-positions line))
+                                                              (length (line-struct-conductor-positions line)))
                                             :initial-element 0d0))
          (image-d-matrix (grid:make-foreign-array 'double-float
-                                                  :dimensions (list (length (cable-struct-conductor-positions cable))
-                                                                    (length (cable-struct-conductor-positions cable)))
+                                                  :dimensions (list (length (line-struct-conductor-positions line))
+                                                                    (length (line-struct-conductor-positions line)))
                                                   :initial-element 0d0))
          (c-matrix (grid:make-foreign-array 'double-float
-                                            :dimensions (list (length (cable-struct-conductor-positions cable))
-                                                              (length (cable-struct-conductor-positions cable)))
+                                            :dimensions (list (length (line-struct-conductor-positions line))
+                                                              (length (line-struct-conductor-positions line)))
                                             :initial-element 0d0))
          (l-vector (grid:make-foreign-array 'double-float
-                                            :dimensions (length (cable-struct-conductor-positions cable))
+                                            :dimensions (length (line-struct-conductor-positions line))
                                             :initial-element 0d0))
          (r-vector (grid:make-foreign-array 'double-float
-                                            :dimensions (length (cable-struct-conductor-positions cable))
+                                            :dimensions (length (line-struct-conductor-positions line))
                                             :initial-element 0d0))
          (k #(0.779d0 0.726d0 0.758d0 0.768d0 0.772d0 0.774d0 0.776d0 0.809d0 0.826d0 0.810d0 1d0))
          (beta (/ (* gsll:+mksa-vacuum-permeability+
-                     (cable-struct-conductor-length cable)
-                     (cable-struct-insulation-permeability cable))
-                  (* 2d0 pi))))
+                     (line-struct-conductor-length line)
+                     (line-struct-insulation-permeability line))
+                  (* 2d0 pi)))
+         (rs nil)
+         (cs nil)
+         (ls nil))
     (labels ((alpha (n i)
                (exp (- (/ (* #c(0d0 1d0) 2d0 pi i) n)))))
       (loop
         with voltage = 0d0
-        for i from 0 below (length (cable-struct-conductor-positions cable))
+        for i from 0 below (length (line-struct-conductor-positions line))
         do
            (loop
              initially (setq voltage 0d0)
-             for j from 0 below (length (cable-struct-conductor-positions cable))
+             for j from 0 below (length (line-struct-conductor-positions line))
              do
-                (setf (grid:gref d-matrix i j) (grid:norm (gsl:elt- (grid:copy (nth i (cable-struct-conductor-positions cable)))
-                                                                    (grid:copy (nth j (cable-struct-conductor-positions cable)))))
-                      (grid:gref image-d-matrix i j) (grid:norm (gsl:elt- (grid:copy (nth i (cable-struct-conductor-positions cable)))
+                (setf (grid:gref d-matrix i j) (grid:norm (gsl:elt- (grid:copy (nth i (line-struct-conductor-positions line)))
+                                                                    (grid:copy (nth j (line-struct-conductor-positions line)))))
+                      (grid:gref image-d-matrix i j) (grid:norm (gsl:elt- (grid:copy (nth i (line-struct-conductor-positions line)))
                                                                           (gsll:elt* (grid:copy-to #(1d0 -1d0) 'grid:foreign-array)
-                                                                                     (grid:copy (nth j (cable-struct-conductor-positions cable)))))))
+                                                                                     (grid:copy (nth j (line-struct-conductor-positions line)))))))
                 (if (= i j)
                     (progn
-                      (setf (grid:gref m-matrix i i) (- (* beta (log (* (aref k (1- (cable-struct-conductor-formation cable)))
-                                                                        (sqrt (/ (nth i (cable-struct-conductor-cross-sections cable))
+                      (setf (grid:gref m-matrix i i) (- (* beta (log (* (aref k (1- (line-struct-conductor-formation line)))
+                                                                        (sqrt (/ (nth i (line-struct-conductor-cross-sections line))
                                                                                  pi))))))
                             (grid:gref a-matrix i i) (* (/ (* 2d0
                                                               pi
                                                               gsll:+mksa-vacuum-permittivity+
-                                                              (cable-struct-insulation-permittivity cable)))
+                                                              (line-struct-insulation-permittivity line)))
                                                         (log (/ (grid:gref image-d-matrix i i)
-                                                                (sqrt (/ (nth i (cable-struct-conductor-cross-sections cable))
+                                                                (sqrt (/ (nth i (line-struct-conductor-cross-sections line))
                                                                          pi))))))
                       (incf voltage (* #c(0d0 -1d0) (grid:gref m-matrix i i))))
                     (progn
@@ -159,42 +165,106 @@
                             (grid:gref a-matrix i j) (* (/ (* 2d0
                                                               pi
                                                               gsll:+mksa-vacuum-permittivity+
-                                                              (cable-struct-insulation-permittivity cable)))
+                                                              (line-struct-insulation-permittivity line)))
                                                         (log (/ (grid:gref image-d-matrix i j)
                                                                 (grid:gref d-matrix i j)))))
                       (incf voltage (* #c(0d0 -1d0)
-                                       (alpha (length (cable-struct-conductor-positions cable))
+                                       (alpha (length (line-struct-conductor-positions line))
                                               (- j i))
                                        (grid:gref m-matrix i j))))))
            (setf (grid:gref l-vector i) (abs (imagpart voltage))
-                 (grid:gref r-vector i) (/ (* (cable-struct-conductor-length cable)
-                                              (cable-struct-conductor-resistivity cable))
-                                           (nth i (cable-struct-conductor-cross-sections cable)))))
+                 (grid:gref r-vector i) (/ (* (line-struct-conductor-length line)
+                                              (line-struct-conductor-resistivity line))
+                                           (nth i (line-struct-conductor-cross-sections line)))))
       (multiple-value-bind (lu-matrix permutation signum)
           (gsll:lu-decomposition (grid:copy a-matrix))
         (declare (ignore signum))
         (let ((gamma-matrix (gsll:lu-invert (grid:copy lu-matrix) permutation)))
           (when gamma-matrix
             (loop
-              for i from 0 below (length (cable-struct-conductor-positions cable))
+              for i from 0 below (length (line-struct-conductor-positions line))
               do
                  (loop
-                   for j from 0 below (length (cable-struct-conductor-positions cable))
+                   for j from 0 below (length (line-struct-conductor-positions line))
                    if (= i j)
                      sum (* (grid:gref gamma-matrix i i)
-                            (alpha (length (cable-struct-conductor-positions cable))
+                            (alpha (length (line-struct-conductor-positions line))
                                    j))
                        into cp
                    else
                      do
                         (setf (grid:gref c-matrix i j) (* (abs (grid:gref gamma-matrix i j))
-                                                          (cable-struct-conductor-length cable)))
+                                                          (line-struct-conductor-length line)))
                    finally (setf (grid:gref c-matrix i i) (* (abs cp)
-                                                             (cable-struct-conductor-length cable))))))
+                                                             (line-struct-conductor-length line))))))
+          (setq rs (/ (loop
+                        for i from 0 below (grid:dim0 r-vector)
+                        sum (grid:gref r-vector i))
+                      (grid:dim0 r-vector))
+                cs (/ (loop
+                        for i from 0 below (grid:dim0 c-matrix)
+                        sum (loop
+                              for j from 0 below (grid:dim1 c-matrix)
+                              sum (grid:gref c-matrix i j)))
+                      (grid:dim0 c-matrix))
+                ls (/ (loop
+                        for i from 0 below (grid:dim0 l-vector)
+                        sum (grid:gref l-vector i))
+                      (grid:dim0 l-vector)))
           (values d-matrix
                   image-d-matrix
                   m-matrix
                   a-matrix
                   r-vector
                   l-vector
-                  c-matrix))))))
+                  c-matrix
+                  (list (make-bipole :kind :capacitance
+                                     :nodes (list 0 1)
+                                     :model-function #'(lambda ())
+                                     :model-parameters (list :value (/ cs 2d0)))
+                        (make-bipole :kind :impedance
+                                     :nodes (list 1 2)
+                                     :model-function #'(lambda ())
+                                     :model-parameters (list :value (complex rs (* 2d0 pi frequency ls))))
+                        (make-bipole :kind :capacitance
+                                     :nodes (list 0 2)
+                                     :model-function #'(lambda ())
+                                     :model-parameters (list :value (/ cs 2d0))))))))))
+                        
+
+;; Utilities
+
+(defun all-permutations (lst &optional (remain lst))
+  (cond ((null remain) nil)
+        ((null (rest lst)) (list lst))
+        (t (append
+            (mapcar (lambda (l) (cons (first lst) l))
+                    (all-permutations (rest lst)))
+            (all-permutations (append (rest lst) (list (first lst))) (rest remain))))))
+
+(defun combinations (a) 
+  (let ((return-value nil))
+    (loop 
+      for i from 0 below (length a) 
+      do
+         (loop
+           for j from i below (length a)
+           do
+              (setq return-value (append return-value (list (list (nth i a) (nth j a)))))))
+    return-value))
+
+(defun rotate (a n)
+  (let ((x nil))
+    (setq x (pop a))
+    (when x
+      (append a (list x)))))
+    
+(defun matrix-trace (a)
+  (check-type a grid:foreign-array)
+  (assert (= (length (grid:dimensions a)) 2))
+  (when (= (grid:dim0 a)
+           (grid:dim1 a))
+    (loop
+      for i from 0 below (grid:dim0 a)
+      sum (grid:gref a i i) into tr
+      finally (return tr))))
